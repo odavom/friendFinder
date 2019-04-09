69782bf0fe45bee64401206855654575077d9bad
Anthony Mastanduno <anthonymastanduno@anthonys-mbp.lan>
2019-04-09 17:24:20 -0400

    added html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+// path package that help with express
+let path = require('path');
+
+// build routes to tell router what pages to deliver based on the url and this will be put in module.exports package so the path can be included in the server
+// pass the variable app through the function (app is express()) and when user hits url /survey then deliver the survey.html
+
+module.exports = function(app) {
+
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname + '/..public/survey.html'));
+    });
+// no exact url but says if using app and not on predefined url then bring to home page
+    app.use(function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
+    })
+
+};
--- server.js
+++ server.js
@@ -9,9 +9,21 @@ let app = express();
 // Set initial port
 let PORT = process.env.PORT || 9000;
 
+// create application/json parser
+let jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+// parse various differnt custon JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }));
+// parse some custon thing into a buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }));
+
+
 // bodyParser to interpret data sent to server
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.text());
 // app.use(bodyParser.json({ type: ''}))
 
@@ -20,7 +32,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // uncomment the below when I have module expots in the files
 // require('./app/routing/apiRoutes.js')(app);
-// require('./app/routing/htmlRoutes.js')(app);
+// require file like require node modules and pass in app in this file into module.exports function
+require('./app/routing/htmlRoutes.js')(app);
 
 // LISTENER - this effectively starts the server
 
